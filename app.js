fed361ffe4bcad3c0535fcc817c05e23cf87cc7a
alerafart <maalejandrarafart@gmail.com>
2022-11-09 19:00:45 +0100

    creation de route post pour creer un article a vendre, en simulant le rajout en bdd car elle n'existe pas encore

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
 const express = require('express');
 
-const app = express();
+const app = express()
 
-//middleware general, qui sera appliqué a toutes les routes
-//toutes requetes
-// on rajoute desz headers a l'objet response
-//permets a l appli d acceder a l api
+// methode/middleware qu'intercepte toutes les requetes q'ont un content type json
+// et mets le corps du contenu a dispo dans req.body
+// equiovalent a l'ancien body-parser
+app.use(express.json())
+
+// middleware general, qui sera appliqué a toutes les routes, donc pas d'adresse en 1er param
+// et a toutes les requetes
+// on rajoute des headers a l'objet response
+// cela permets a l'appli d'acceder a l'API
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-    app.use('/api/stuff', (req, res, next) => {
-        const stuff = [
-          {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-          },
-          {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-          },
-        ];
-        res.status(200).json(stuff);
-      });
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    next()
+});
+
+app.post('/api/stuff', (req, res, next)=> {
+    console.log(req.body)
+    res.status(201).json({
+        message: 'Objet crée!'
+    })
+
+})
+
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+        },
+        {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+        },
+    ]
+    res.status(200).json(stuff)
+})
 
 module.exports = app;
