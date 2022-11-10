c9949b0ae642239e115867452d07d68f8721241d
alerafart <maalejandrarafart@gmail.com>
2022-11-10 14:47:36 +0100

    route get to retrieve one single object from its id

--- app.js
+++ app.js
@@ -38,10 +38,18 @@ app.post('/api/stuff', (req, res, next)=> {
         .catch(error => res.status(400).json({ error }))
 })
 
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({_id: req.params.id})
+    .then(thing => res.status(200).json(thing)) //promise
+    .catch(error => res.status(404).json({error}))
+})
+
 app.get('/api/stuff', (req, res, next) => {
     Thing.find()
     .then(things => res.status(200).json(things)) //promise
     .catch(error => res.status(400).json({error}))
 })
+// La base de données MongoDB est fractionnée en collections : le nom de la collection est défini par défaut sur le pluriel du nom du modèle. Ici, ce sera Things
+
 
 module.exports = app;
