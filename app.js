8ef3cf004cfc0802fb83e186c3dabaa3050597ef
alerafart <maalejandrarafart@gmail.com>
2022-11-10 15:03:53 +0100

    delete one object

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
     next()
 });
 
+// methode pour créer un objet
 app.post('/api/stuff', (req, res, next)=> {
     delete req.body._id //j'efface le champ id car il sera generé automatiquuement par la bdd
     const thing = new Thing({
@@ -38,18 +39,28 @@ app.post('/api/stuff', (req, res, next)=> {
         .catch(error => res.status(400).json({ error }))
 })
 
+// methode pour modifier un objet
 app.put('/api/stuff/:id', (req, res, next)=> {
     Thing.updateOne({_id: req.params.id}, { ...req.body, _id: req.params.id})
     .then(res.status(200).json({ message: 'Objet modifié !'}))
     .catch(error => res.status(400).json({ error }))
 })
 
+// methode pour suprimer un objet
+app.delete('/api/stuff/:id', (req, res, next)=> {
+    Thing.deleteOne({_id: req.params.id}, { ...req.body, _id: req.params.id})
+    .then(res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }))
+})
+
+// methode pour retrouver un objet par id
 app.get('/api/stuff/:id', (req, res, next) => {
     Thing.findOne({_id: req.params.id})
     .then(thing => res.status(200).json(thing)) //promise
     .catch(error => res.status(404).json({error}))
 })
 
+// methode pour retrouver tous les objets
 app.get('/api/stuff', (req, res, next) => {
     Thing.find()
     .then(things => res.status(200).json(things)) //promise
