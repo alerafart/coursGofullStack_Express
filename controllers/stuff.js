ca03beb2697bc5fbae2415acd0f8b1b243bbacdd
alerafart <maalejandrarafart@gmail.com>
2022-11-11 00:09:44 +0100

    factorisation, routes and CRUD methods splitted in different files, routes and controllers

---
+++ controllers/stuff.js
@@ -0,0 +1,43 @@
+const Thing = require('../models/Thing')
+
+// methode pour créer un objet
+exports.createThing = (req, res, next)=> {
+    delete req.body._id //j'efface le champ id car il sera generé automatiquuement par la bdd
+    const thing = new Thing({
+    title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    })
+    thing.save()
+        .then(() => res.status(201).json({message: 'objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }))
+}
+
+// methode pour modifier un objet
+exports.modifyThing = (req, res, next)=> {
+    Thing.updateOne({_id: req.params.id}, { ...req.body, _id: req.params.id})
+    .then(res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }))
+}
+
+// methode pour suprimer un objet
+exports.deleteThing = (req, res, next)=> {
+    Thing.deleteOne({_id: req.params.id}, { ...req.body, _id: req.params.id})
+    .then(res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }))
+}
+
+// methode pour retrouver un objet par id
+exports.getOneThing =(req, res, next) => {
+    Thing.findOne({_id: req.params.id})
+    .then(thing => res.status(200).json(thing)) //promise
+    .catch(error => res.status(404).json({error}))
+}
+
+exports.getAllThings = (req, res, next) => {
+    Thing.find()
+    .then(things => res.status(200).json(things)) //promise
+    .catch(error => res.status(400).json({error}))
+}
